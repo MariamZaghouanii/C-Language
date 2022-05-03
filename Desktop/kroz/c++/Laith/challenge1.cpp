5a6392180c9a32dd82c38c7cf0baf1d262f3ee1b
LaithZoghlami <laithzoghlami23@gmail.com>
2022-05-03 17:04:31 +0100

    the pro x max ultimate update

--- Desktop/kroz/c++/Laith/challenge1.cpp
+++ Desktop/kroz/c++/Laith/challenge1.cpp
@@ -4,34 +4,43 @@ using namespace std;
 
 int main()
 {
-    int a;
-    cout << "Type the 1st number : ";
-    cin >> a;
-    int b;
-    cout << "Type the 2nd number : ";
-    cin >> b;
-    int res;
-    cout << "Addition '+'" << endl;
-    cout << "Subtraction '-'" << endl;
-    cout << "Product '*' " << endl;
-    cout << "Division '/'" << endl;
-    char choice;
-    cout << "Choose an operation : ";
-    cin >> choice;
-    if ( choice == '+'){
-        res = a + b;
-        cout << "The result = " << res << endl;
-    } else if( choice == '-'){
-        res = a - b;
-        cout << "The result = " << res << endl;
-    } else if( choice == '*'){
-        res = a * b;
-        cout << "The result = " << res << endl;
-    } else if( choice == '/'){
-        res = a / b;
-        cout << "The result = " << res << endl;
-    } else{
-        cout << "There was an error choosing the operation" << endl;
+    bool eff = true;
+    while ( eff == true){
+        int a;
+        cout << "Type the 1st number : ";
+        cin >> a;
+        int b;
+        cout << "Type the 2nd number : ";
+        cin >> b;
+        int res;
+        cout << "Addition '+'" << endl;
+        cout << "Subtraction '-'" << endl;
+        cout << "Product '*' " << endl;
+        cout << "Division '/'" << endl;
+        char choice;
+        cout << "Choose an operation : ";
+        cin >> choice;
+        if ( choice == '+'){
+            res = a + b;
+            cout << "The result = " << res << endl;
+            eff = false;
+        } else if( choice == '-'){
+            res = a - b;
+            cout << "The result = " << res << endl;
+            eff = false;
+        } else if( choice == '*'){
+            res = a * b;
+            cout << "The result = " << res << endl;
+            eff = false;
+        } else if( choice == '/'){
+            res = a / b;
+            cout << "The result = " << res << endl;
+            eff = false;
+        } else{
+            cout << "There was an error choosing the operation" << endl;
+            
+        }
+    
     }
     
 

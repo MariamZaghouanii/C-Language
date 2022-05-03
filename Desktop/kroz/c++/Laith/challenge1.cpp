60a97935034b3134b783ab813649fa040a2be0a6
LaithZoghlami <laithzoghlami23@gmail.com>
2022-05-03 17:35:20 +0100

    sayb zeby

--- Desktop/kroz/c++/Laith/challenge1.cpp
+++ Desktop/kroz/c++/Laith/challenge1.cpp
@@ -4,44 +4,78 @@ using namespace std;
 
 int main()
 {
-    bool eff = true;
-    while ( eff == true){
-        int a;
+    int a,b,res;
+    char choice;
+
+    cout << "Type the 1st number : ";
+    cin >> a;
+    cout << isdigit(a);
+
+
+
+
+
+
+
+   /*  while (isdigit(a) == false){
+        cout << "1st number is not integer" << endl;
         cout << "Type the 1st number : ";
         cin >> a;
-        int b;
+
+    }
+
+
+
+    cout << "Type the 2nd number : ";
+    cin >> b;
+    while (isdigit(b) == false){
+        cout << "2nd number is not integer" << endl;
         cout << "Type the 2nd number : ";
         cin >> b;
-        int res;
-        cout << "Addition '+'" << endl;
-        cout << "Subtraction '-'" << endl;
-        cout << "Product '*' " << endl;
-        cout << "Division '/'" << endl;
-        char choice;
+    }
+
+
+
+
+
+    cout << "Addition '+'" << endl;
+    cout << "Subtraction '-'" << endl;
+    cout << "Product '*' " << endl;
+    cout << "Division '/'" << endl;
+
+
+
+    
+    cout << "Choose an operation : ";
+    cin >> choice;
+    while ( (choice != '+') && ( choice != '-') && ( choice != '*') && ( choice != '/') ){
+        cout << "Invalid Input" << endl;
         cout << "Choose an operation : ";
         cin >> choice;
-        if ( choice == '+'){
-            res = a + b;
-            cout << "The result = " << res << endl;
-            eff = false;
-        } else if( choice == '-'){
-            res = a - b;
-            cout << "The result = " << res << endl;
-            eff = false;
-        } else if( choice == '*'){
-            res = a * b;
-            cout << "The result = " << res << endl;
-            eff = false;
-        } else if( choice == '/'){
-            res = a / b;
-            cout << "The result = " << res << endl;
-            eff = false;
-        } else{
-            cout << "There was an error choosing the operation" << endl;
-            
-        }
-    
     }
+
+
+
+    if ( choice == '+'){
+        res = a + b;
+        cout << "The result = " << res << endl;
+        
+    } else if( choice == '-'){
+        res = a - b;
+        cout << "The result = " << res << endl;
+        
+    } else if( choice == '*'){
+        res = a * b;
+        cout << "The result = " << res << endl;
+        
+    } else {
+        res = a / b;
+        cout << "The result = " << res << endl;
+    } */
+        
+       
+    
+
     
 
 
